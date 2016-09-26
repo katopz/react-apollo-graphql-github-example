7701c72e09b25b60fc5db7b5f1e0a2c0a25344db
Todsaporn (katopz) Banjerdkit <todsaporn.banjerdkit@thomsonreuters.com>
2016-09-26 15:29:57 +0700

    Add missing ul

--- src/repository.js
+++ src/repository.js
@@ -75,8 +75,10 @@ class Repository extends React.Component {
   render() {
     return (<div>
       <h2>{this.state.login}/{this.state.name}</h2>
-      <li>stargazers: {this.state.stargazers.toLocaleString()}</li>
-      <li>watchers: {this.state.watchers.toLocaleString()}</li>
+      <ul>
+        <li>stargazers: {this.state.stargazers.toLocaleString() }</li>
+        <li>watchers: {this.state.watchers.toLocaleString() }</li>
+      </ul>
     </div>)
   }
 }
